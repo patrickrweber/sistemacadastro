e95c4d87cc205ab3e9519cdaa8ea8eff732148de
patrickrweber <patrickrenanweber@gmail.com>
2022-10-04 21:11:47 -0300

    Cadastro das classes

---
+++ SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Data/ConvenioRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms.Data
+{
+    public class ConvenioRepository
+    {
+
+        #region SQLS
+        private const String SQL_SELECT_CONVENIO = "SELECT * FROM convenio";
+        #endregion
+    }
+}
